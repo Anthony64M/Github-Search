bb3a9137020237bd856adc98fd2f839931546aae
Anthony64M <anthony.mutuku@student.moringaschool.com>
2021-08-29 22:26:32 +0300

    Added the ts after adding the imprt paths of the various files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import {HttpClientModule} from '@angular/common/http';
+import { TimeAgoPipe } from './time-ago.pipe';
+import { ReposComponent } from './components/repos/repos.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    TimeAgoPipe,
+    ReposComponent,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
